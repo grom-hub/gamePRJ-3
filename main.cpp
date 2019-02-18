84add399a0ce1b87fd67d197e0b42e1a5654014d
Yaroslav <ya4ule@gmail.com>
2019-02-18 14:33:32 +0300

    v1

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <ncurses.h>
 #include <unistd.h> // usleep()
 #include <cstdlib> // rand(), srand(), exit()
-//#include <ctime> // time()
+#include <ctime> // time()
 //#include <string>
 //#include <vector>
 
@@ -15,6 +15,12 @@ int main()
 	int pX = 5;
 	int pY = 5;
 
+	int moveId = 0;
+	int cTest = 0;
+
+	char wSkin = '#';
+	int wX[10];
+	int wY[10];
 
 
 	if (!initscr())
@@ -29,27 +35,91 @@ int main()
     clear();
     refresh();
 
-	
+	srand(time(0));
+
+
+
+    for(int i = 0; i < 10; i++)
+    {
+    	wX[i] = 1 + rand() % 20;
+    	wY[i] = 1 + rand() % 20;
+    }
+
 
 	while(key != 'q')
 	{
 		usleep(10000);
 
 		clear();
-		box(stdscr, 0,0);		
+		box(stdscr, 0,0);
+		for(int i = 0; i < 10; i++)
+		{
+			mvaddch(wX[i], wY[i], wSkin);					
+		}
 		mvaddch(pX, pY, pSkin);
 		refresh();
 
 		key = getch();
-		if (key == KEY_DOWN && pX < LINES-2)
+		moveId = 0;
+		if (key == KEY_DOWN)
+		{
 			pX++;
-		if (key == KEY_UP && pX > 1)
+			moveId = 1;
+		}
+		if (key == KEY_UP)
+		{
 			pX--;
-		if (key == KEY_RIGHT && pY < COLS-3)
+			moveId = 2;
+		}
+		if (key == KEY_RIGHT)
+		{
 			pY++;
-		if (key == KEY_LEFT && pY > 2)
+			moveId = 3;
+		}
+		if (key == KEY_LEFT)
+		{
 			pY--;
+			moveId = 4;
+		}
 
+
+		if(moveId != 0) // Если было движение, проверить не столкновение.
+		{
+			cTest = 0;
+
+			if(pX > LINES-2 || pX < 1 || pY > COLS-3 || pY < 2)
+			{
+				cTest = 1;
+			}
+			
+			for (int i = 0; i < 10; i++)
+			{
+				if(pX == wX[i] && pY == wY[i])
+				{
+					cTest = 1;
+					break;
+				}
+			}
+		}
+
+		if(cTest == 1) // Если обнаружено столкновение, отменить движение.
+		{
+			switch (moveId)
+			{
+				case 1:
+					pX--;
+					break;
+				case 2:
+					pX++;
+					break;
+				case 3:
+					pY--;
+					break;
+				case 4:
+					pY++;
+					break;
+			}
+		}
 	}
 
 
