b230e9b7cccacc4ccb5d60fcd8e0f97121bf4f14
Yaroslav <ya4ule@gmail.com>
2019-02-18 15:15:22 +0300

    v2

--- main.cpp
+++ main.cpp
@@ -10,10 +10,11 @@
 int main()
 {
 	int key;
+	int slct = 0;
 
-	char pSkin = 'A';
-	int pX = 5;
-	int pY = 5;
+	char pSkin[3] = {'A', 'B', 'C'};
+	int pX[3] = {2, 4, 6};
+	int pY[3] = {2, 4, 6};
 
 	int moveId = 0;
 	int cTest = 0;
@@ -39,7 +40,7 @@ int main()
 
 
 
-    for(int i = 0; i < 10; i++)
+    for(int i = 0; i < 10; i++) // Расстановка стен
     {
     	wX[i] = 1 + rand() % 20;
     	wY[i] = 1 + rand() % 20;
@@ -48,78 +49,102 @@ int main()
 
 	while(key != 'q')
 	{
-		usleep(10000);
+		usleep(10000); // Частота обновления.
 
+/////////////////// Вывод на экран.
 		clear();
 		box(stdscr, 0,0);
 		for(int i = 0; i < 10; i++)
 		{
 			mvaddch(wX[i], wY[i], wSkin);					
 		}
-		mvaddch(pX, pY, pSkin);
+		for (int i = 0; i < 3; i++)
+		{
+		mvaddch(pX[i], pY[i], pSkin[i]);
+		}
 		refresh();
+/////////////////
 
-		key = getch();
+/////////////////// Проверка нажатых клавиш.
+		key = getch(); 
 		moveId = 0;
+
 		if (key == KEY_DOWN)
 		{
-			pX++;
+			pX[slct]++;
 			moveId = 1;
 		}
 		if (key == KEY_UP)
 		{
-			pX--;
+			pX[slct]--;
 			moveId = 2;
 		}
 		if (key == KEY_RIGHT)
 		{
-			pY++;
+			pY[slct]++;
 			moveId = 3;
 		}
 		if (key == KEY_LEFT)
 		{
-			pY--;
+			pY[slct]--;
 			moveId = 4;
 		}
+		if (key == ' ')
+		{
+			slct++;
+			if (slct > 2)
+				slct = 0;
+		}
+///////////////////
 
-
-		if(moveId != 0) // Если было движение, проверить не столкновение.
+/////////////////// Если было движение, проверить не столкновение.
+		if(moveId != 0)
 		{
 			cTest = 0;
 
-			if(pX > LINES-2 || pX < 1 || pY > COLS-3 || pY < 2)
+			if(pX[slct] > LINES-2 || pX[slct] < 1 || pY[slct] > COLS-3 || pY[slct] < 2)
 			{
 				cTest = 1;
 			}
 			
 			for (int i = 0; i < 10; i++)
 			{
-				if(pX == wX[i] && pY == wY[i])
+				if(pX[slct] == wX[i] && pY[slct] == wY[i])
+				{
+					cTest = 1;
+					break;
+				}
+			}
+
+			for (int i = 0; i < 3; i++)
+			{
+				if(slct != i && pX[slct] == pX[i] && pY[slct] == pY[i])
 				{
 					cTest = 1;
 					break;
 				}
 			}
 		}
-
-		if(cTest == 1) // Если обнаружено столкновение, отменить движение.
+/////////////////// Если обнаружено столкновение, отменить движение.
+		if(cTest == 1)
 		{
 			switch (moveId)
 			{
 				case 1:
-					pX--;
+					pX[slct]--;
 					break;
 				case 2:
-					pX++;
+					pX[slct]++;
 					break;
 				case 3:
-					pY--;
+					pY[slct]--;
 					break;
 				case 4:
-					pY++;
+					pY[slct]++;
 					break;
 			}
 		}
+///////////////////
 	}
 
 
